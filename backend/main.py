deab4e5b00ee603a66f9a3ec9bee4066b6a6cb87
priyanshush325 <priyanshush325@gmail.com>
2024-06-22 12:47:27 -0700

    set up firebase

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+import requests
+from firebase import firebase
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+firebase = firebase.FirebaseApplication('https://calhacks-2024-default-rtdb.firebaseio.com/', None)
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = firebase.get('/people', None)
+    return jsonify(people)
+
+if __name__ == "__main__":
+    app.run(debug = True)
