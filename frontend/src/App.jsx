3ed70d696ff0c2c4198cb8fa74f11ff424628838
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-06-23 13:30:03 -0700

    remove about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -17,16 +16,12 @@ function App() {
                     </Link>
                     <Link to="/contact" className="ml-4">
                         Contact
-                        <Link to="/about" className="ml-4">
-                            About
-                        </Link>
                     </Link>
                 </div>
             </div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/projects" element={<ProjectsPage />} />
-                <Route path="/about" element={<AboutPage />} />
                 <Route path="/contact" element={<ContactPage />} />
             </Routes>
         </Router>
