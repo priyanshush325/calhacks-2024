cb11dd12774aa36aa2bd28954224acdcb18b9c74
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-06-22 20:48:30 -0700

    context and commands!!!

---
+++ frontend/src/components/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+const ProfilePage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1>Profile</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
