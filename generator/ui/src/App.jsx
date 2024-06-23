ed8bce2caf7d0cd68582b2638fb81166daecb6f0
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-06-23 02:16:26 -0700

    finish generator UI

--- generator/ui/src/App.jsx
+++ generator/ui/src/App.jsx
@@ -1,58 +1,120 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import TextareaAutosize from "react-textarea-autosize";
 import "./App.css";
 
 const baseUrl = import.meta.env.VITE_PROJECT_URL;
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
+async function sendMessage(message) {
+	response = await fetch(serverUrl + "/message", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ message }),
+	});
+}
+
+async function sendProject(project) {
+	response = await fetch(serverUrl + "/project", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: {
+			projectName: project.name,
+			projectSourceDir: project.path,
+		},
+	});
+}
 
 function App() {
+	const [project, setProject] = useState({
+		name: "It literally doesn't matter",
+		path: "",
+	});
+	const [projectSet, setProjectSet] = useState(false);
+
 	const [url, setUrl] = useState(baseUrl);
-
 	const [urlBar, setUrlBar] = useState(baseUrl);
-
 	const [message, setMessage] = useState("");
 
 	return (
 		<>
 			<div className="page">
-				<div>
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							setUrl(urlBar);
-						}}
-					>
-						<input
-							className="url-bar"
-							type="text"
-							value={urlBar}
-							onChange={(e) => setUrlBar(e.target.value)}
-						/>
-					</form>
-				</div>
 				<div className="content">
-					<iframe
-						src={url}
-						title="iframe"
-						// width="100%"
-						id="iframe"
-					></iframe>
-					<div className="chat">
+					<div>
 						<form
+							className="url-bar-container"
 							onSubmit={(e) => {
 								e.preventDefault();
-								console.log(message);
+								setUrl(urlBar);
 							}}
 						>
-							<textarea
-								className="message-box"
+							<input
+								className="url-bar"
 								type="text"
-								placeholder="Type a message"
-								value={message}
-								onChange={(e) => setMessage(e.target.value)}
+								value={urlBar}
+								onChange={(e) => setUrlBar(e.target.value)}
 							/>
 						</form>
 					</div>
+					<div className="iframe-container">
+						{projectSet && (
+							<iframe
+								src={url}
+								title="iframe"
+								// width="100%"
+								id="iframe"
+							></iframe>
+						)}
+						{!projectSet && (
+							<div className="chat-settings">
+								<h1>Select a Project</h1>
+								{/* <input
+									type="text"
+									placeholder="Project name"
+									onChange={(e) =>
+										setProject({ ...project, name: e.target.value })
+									}
+								/> */}
+								<input
+									type="text"
+									placeholder="Absolute project path"
+									onChange={(e) =>
+										setProject({ ...project, path: e.target.value })
+									}
+								/>
+								<button>Submit</button>
+							</div>
+						)}
+					</div>
+				</div>
+				<div className="chat">
+					<div
+						style={{
+							flex: 1,
+						}}
+					></div>
+
+					<TextareaAutosize
+						className="message-box"
+						type="text"
+						placeholder="Enter a prompt..."
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								e.preventDefault();
+
+								console.log("Enter pressed");
+								if (message === "") return;
+
+								sendMessage(message);
+								setMessage("");
+							}
+						}}
+					/>
 				</div>
 			</div>
 		</>
