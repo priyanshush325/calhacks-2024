04eb395d6f6f4a6ea86fde3f1cebff80745ed6a7
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-06-22 20:02:15 -0700

    reset to hello world

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import CalculatorComponent from "./components/CalculatorComponent.jsx";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-full">
-            <CalculatorComponent />
+            <h1 className="text-2xl">Hello World</h1>
         </div>
     );
 }
